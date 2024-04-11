2fa379483ab3c526f248c70fcaf66d05b321462a
yurcheshka <ura19092004@gmail.com>
2024-04-11 16:32:02 +0300

    feat: add table to category page

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {MatTable, MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { HeaderComponent } from '../header/header.component';
 import { MatButton } from '@angular/material/button';
 import { CategoryRowComponent } from '../category-row/category-row.component';
-import { PaginatorComponent } from '../paginator/paginator.component';
+import { MyCustomPaginatorIntl, PaginatorComponent } from '../paginator/paginator.component';
+import { CategoryServiceService } from '../../services/Category/category-service.service';
+import { Category } from '../../models/category/category';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [MatButton, HeaderComponent, CategoryRowComponent, PaginatorComponent],
+  imports: [MatButton, PaginatorComponent, MatTable, MatTableModule, MatPaginator, MatPaginatorModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
+export class CategoryComponent implements AfterViewInit{ 
+  categories : Category[] = [];
+  dataSource = new MatTableDataSource<Category>(this.categories);
+  displayedColumns : string[] = ['id', 'name'];
 
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  ngOnInit() {
+    // this.categoryService.getCategories().subscribe({next:(data: any[]) => this.categories = data});
+  }
+
+  ngAfterViewInit() {
+    this.paginator._intl = new MyCustomPaginatorIntl;
+    this.dataSource.paginator = this.paginator;
+  }
 }
