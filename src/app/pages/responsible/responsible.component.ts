405c398e07f1f9fdc8f3f3de9826d0e7c5dad57a
yurcheshka <ura19092004@gmail.com>
2024-04-11 17:29:20 +0300

    feat: add table to responsible page

--- src/app/pages/responsible/responsible.component.ts
+++ src/app/pages/responsible/responsible.component.ts
@@ -1,29 +1,55 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { PaginatorComponent } from '../paginator/paginator.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { ResponsibleRowComponent } from '../responsible-row/responsible-row.component';
-import { HeaderComponent } from '../header/header.component';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MyCustomPaginatorIntl } from '../paginator/paginator.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Responsible } from '../../models/responsible/responsible';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 @Component({
   selector: 'app-responsible',
   standalone: true,
-  imports: [MatToolbarModule, 
+  imports: [
+    MatToolbarModule, 
     MatIconModule, 
     MatButtonModule, 
     FormsModule, 
     MatInputModule,
     MatFormFieldModule, 
     MatExpansionModule, 
-    PaginatorComponent, MatCheckboxModule, ResponsibleRowComponent, HeaderComponent],
+    MatCheckboxModule, 
+    MatPaginatorModule,
+    MatTableModule
+  ],
   templateUrl: './responsible.component.html',
   styleUrl: './responsible.component.css'
 })
-export class ResponsibleComponent {
+export class ResponsibleComponent implements AfterViewInit {
+  responsibleData : Responsible[] = [];
+  dataSource = new MatTableDataSource<Responsible>(this.responsibleData);
+  displayedColumns : string[] = [
+    'id', 
+    'lastName', 
+    'firstName', 
+    'patronymic',
+    'position',
+    'phoneNumber',
+    'isFinanciallyResponsible',
+    'department'
+  ];
 
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  ngOnInit() {
+    this.paginator._intl = new MyCustomPaginatorIntl;
+  }
+
+  ngAfterViewInit(): void {  
+    this.dataSource.paginator = this.paginator;
+  }
 }
