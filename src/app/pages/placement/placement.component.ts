4d3243e26fd5168052a7f750e0222c2c2a0460a1
yurcheshka <ura19092004@gmail.com>
2024-04-11 16:39:51 +0300

    feat: add table to placement page

--- src/app/pages/placement/placement.component.ts
+++ src/app/pages/placement/placement.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { PlacementRowComponent } from '../placement-row/placement-row.component';
-import { PaginatorComponent } from '../paginator/paginator.component';
-
+import { MyCustomPaginatorIntl, PaginatorComponent } from '../paginator/paginator.component';
+import { Placement } from '../../models/placement/placement';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @Component({
   selector: 'app-placement',
   standalone: true,
-  imports: [HeaderComponent, MatButtonModule, MatButton, PlacementRowComponent, PaginatorComponent],
+  imports: [HeaderComponent, MatButtonModule, MatButton, MatPaginatorModule, MatTableModule],
   templateUrl: './placement.component.html',
   styleUrl: './placement.component.css'
 })
 export class PlacementComponent {
+  placements : Placement[] = [new Placement(1, 'Помещение 1'), new Placement(2, 'Помзеение 2')];
+  dataSource = new MatTableDataSource<Placement>(this.placements);
+  displayedColumns : string[] = ['id', 'name'];
 
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  ngOnInit() {
+    // this.categoryService.getCategories().subscribe({next:(data: any[]) => this.categories = data});
+  }
+
+  ngAfterViewInit() {
+    this.paginator._intl = new MyCustomPaginatorIntl;
+    this.dataSource.paginator = this.paginator;
+  }
 }
