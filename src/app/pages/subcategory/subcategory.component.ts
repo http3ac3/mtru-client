5d42791a5a67b38a39c3e800977369077e1b7645
yurcheshka <ura19092004@gmail.com>
2024-04-11 17:10:53 +0300

    feat: add table to subcategory page

--- src/app/pages/subcategory/subcategory.component.ts
+++ src/app/pages/subcategory/subcategory.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatExpansionModule, MatExpansionPanel} from '@angular/material/expansion';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatButton } from '@angular/material/button';
-import { SubcategoryRowComponent } from '../subcategory-row/subcategory-row.component';
-import { PaginatorComponent } from '../paginator/paginator.component';
+import { Subcategory } from '../../models/subcategory/subcategory';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-subcategory',
   standalone: true,
-  imports: [HeaderComponent, MatExpansionModule, MatCheckbox, MatButton, SubcategoryRowComponent, PaginatorComponent],
+  imports: [MatExpansionModule, MatCheckbox, MatButton, MatPaginator, MatPaginatorModule, MatTableModule],
   templateUrl: './subcategory.component.html',
   styleUrl: './subcategory.component.css'
 })
-export class SubcategoryComponent {
+export class SubcategoryComponent implements AfterViewInit{
+  subcategories : Subcategory[] = [];
+  dataSource = new MatTableDataSource<Subcategory>(this.subcategories);
+  displayedColumns : string[] = ['id', 'name', 'category'];
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor () {}
+
+  ngOnInit() {
+    // this.subcategoryService.getSubcategories().subscribe({next:(data: any[]) => this.subcategories = data});
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;    
+  }
 
 }
