1926846c5c16ea2d421cd47915a62d0f8310bc3f
yurcheshka <ura19092004@gmail.com>
2024-05-07 13:53:53 +0300

    feat: rents can't be deleted

--- src/app/pages/dialogs/rent-information-dialog/rent-information-dialog.component.ts
+++ src/app/pages/dialogs/rent-information-dialog/rent-information-dialog.component.ts
@@ -79,20 +79,20 @@ export class RentInformationDialogComponent {
     this.dialogRef.close();
   }
 
-  onRentDelete() {
-    let rent = this.rentForm.value;
-    if (!confirm(`Вы уверены, что хотите удалить выбранное взятие пользователя?`)) return;
-    console.log(rent);
-    this.rentService.delete(rent.id).subscribe({
-      complete: () => {
-        alert(`Взятие оборудования ${rent.name} (инв. № ${rent.inventoryNumber}) успешно удалено!`);
-        this.dialogRef.close();
-        window.location.reload();
-      },
-      error: (err) => {
-        if (err.status == 423) alert('Ошибка! Удалить взятие, пока оно не завершено, невозможно!');
-        else alert('Непредвиденная ошибка! Удалить взятие')
-      }
-    })
-  }
+  // onRentDelete() {
+  //   let rent = this.rentForm.value;
+  //   if (!confirm(`Вы уверены, что хотите удалить выбранное взятие пользователя?`)) return;
+  //   console.log(rent);
+  //   this.rentService.delete(rent.id).subscribe({
+  //     complete: () => {
+  //       alert(`Взятие оборудования ${rent.name} (инв. № ${rent.inventoryNumber}) успешно удалено!`);
+  //       this.dialogRef.close();
+  //       window.location.reload();
+  //     },
+  //     error: (err) => {
+  //       if (err.status == 423) alert('Ошибка! Удалить взятие, пока оно не завершено, невозможно!');
+  //       else alert('Непредвиденная ошибка! Удалить взятие')
+  //     }
+  //   })
+  // }
 }
